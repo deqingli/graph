8354511b0207d53212a665bfaebfed515b9135ce
deqingli <annong035@gmail.com>
2017-12-28 11:35:01 +0800

    add flowgraph series and pachage.json

--- src/FlowGraphSeries.js
+++ src/FlowGraphSeries.js
@@ -2,7 +2,18 @@ var echarts = require('echarts/lib/echarts');
 
 echarts.extendSeriesModel({
     type: 'series.flowGraph',
+
+    getInitialData: function(option, ecModel) {
+        var data = option.data;
+        var nodes = data.node.concat(data.input, data.output) || [];
+        var edges = data.edge || [];
+        return {
+            nodes: nodes,
+            edges: edges
+        };
+    },
     
-
-
+    defaultOption: {
+        rankDir: "LR"  
+    }
 });
